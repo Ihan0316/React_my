0ebd87629a4ac0e99de1265dba321da52ee2ab96
“Ihan0316” <aijnjj12@gmail.com>
2025-01-14 15:36:22 +0900

    수정

--- vite-test/src/App.jsx
+++ vite-test/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 
 function App() {
+  const name = '리액트';
   // jsx 문법은 우리가 보기에는 HTML로 보이지만
   // 실제 문법은
   // return React.createElement("div", null, "Hello ", React.createElement ("b", null, "react"));
   return (
     <>
       {/* <fragment> */}
-      <h1>Hello React 안녕</h1>
-      <h2>잘 작동 하니?</h2>
-      <div>수정</div>
+      <h1>Hello React</h1>
+      <h2>잘 작동 하니? {name}</h2>
       {/* </fragment> */}
     </>
   );
