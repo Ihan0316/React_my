697c797d02e29673e0a08bfcda71beb6e41f1d46
“Ihan0316” <aijnjj12@gmail.com>
2025-01-15 10:50:39 +0900

    25/1/15 2교시

--- ch3-vite-test/src/App.jsx
+++ ch3-vite-test/src/App.jsx
@@ -8,7 +8,14 @@ function App() {
       <h1>{name}</h1>
       <div className="react">{name}</div>
       {/* 자식 컴포넌트 */}
-      <MyComponent name="데이터를 부모에서 자식으로 ㅈ, 속성은 name으로 전달" />
+      {/* 1, props 이용해서 데이터 전달 */}
+      {/* <MyComponent name="데이터를 부모에서 자식으로, 속성은 name으로 전달" /> */}
+      {/* 2, props를 전달할 때 defaultProps 설정 */}
+      {/* <MyComponent /> */}
+      {/* 3, children 이용해서 데이터 전달 */}
+      <MyComponent name="동시에 같이 보내기">
+        children 요소로 데이터 전달하기
+      </MyComponent>
     </>
   );
 }
--- ch3-vite-test/src/component/MyComponent.jsx
+++ ch3-vite-test/src/component/MyComponent.jsx
@@ -1,16 +1,22 @@
 const MyComponent = (props) => {
+  // 비구조화 할당 , 구조분해,
+  // name = props.name, children = props.children
+  const { name, children } = props;
   return (
-    <>
-      <div>
-        <h2>새로운 컴포넌트</h2>
-        <h2>데이터 전달받기 : {props.name}</h2>
-      </div>
-    </>
+    <div>
+      샘플 나의 첫 컴포넌트 만들기.
+      <h2>순서1, 부모 컴포넌트에서 전달 받은 props 데이터를 사용하기.</h2>
+      {/* <h3>데이터 전달 받기 props.name : {props.name}</h3> */}
+      <h3>데이터 전달 받기 props.name : {name}</h3>
+      <h2>순서2, 부모 컴포넌트에서 전달 받은 children 데이터를 사용하기.</h2>
+      {/* <h3>데이터 전달 확인 props.children : {props.children}</h3> */}
+      <h3>데이터 전달 확인 props.children : {children}</h3>
+    </div>
   );
 };
 
 MyComponent.defaultProps = {
-  name: '기본값',
+  name: '기본 이름',
 };
 
 export default MyComponent;
