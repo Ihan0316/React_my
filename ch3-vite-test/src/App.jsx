a3407eff626dd9754989f7a2c512054c0e4f6d82
“Ihan0316” <aijnjj12@gmail.com>
2025-01-15 12:37:49 +0900

    25/1/15 4교시&미니실습

--- ch3-vite-test/src/App.jsx
+++ ch3-vite-test/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
 import Counter from './component/Counter';
+import Mini1 from './component/Mini1';
 import MyComponent from './component/MyComponent';
 import MyComponent2 from './component/MyComponent2';
+import Say from './component/Say';
 
 function App() {
   const name = '리액트';
@@ -23,7 +25,11 @@ function App() {
       {/* 6, 클래스형 컴포넌트로 확인하기 */}
       {/* <MyComponent2 name="동시에 데이터 전달" favoriteNumber={100}>children 요소로 데이터 전달하기</MyComponent2> */}
       {/* 7, 클래스형 컴포넌트에서 state 확인 */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* 8, 함수형 컴포넌트에서 hooks -> useState 확인 */}
+      {/* <Say /> */}
+      {/* 미니실습1 */}
+      <Mini1 />
     </>
   );
 }
---
+++ ch3-vite-test/src/component/Mini1.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const Mini1 = () => {
+  const [message, setMessage] = useState('');
+  const [color, setColor] = useState('black');
+  const [user, setUser] = useState('');
+
+  const onClickEnter = () => setMessage('안녕하세요!');
+  const onClickLeave = () => setMessage('안녕히 가세요!');
+  const onClickWelcome = () => setMessage('환영합니다!');
+  const onClickUser = () => setMessage(user);
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <button onClick={onClickWelcome}>환영</button>
+      <div>
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <button onClick={onClickUser}>사용자</button>
+      </div>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        초록색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
+      <button style={{ color: 'black' }} onClick={() => setColor('black')}>
+        검은색
+      </button>
+    </div>
+  );
+};
+
+export default Mini1;
