ea2adda8062c95f0601b2f9952e681ca41e45a3f
“Ihan0316” <aijnjj12@gmail.com>
2025-01-20 11:25:49 +0900

    25/1/20 3교시

--- ch9-vite-test/src/App.jsx
+++ ch9-vite-test/src/App.jsx
@@ -1,11 +1,30 @@
+import { Component } from 'react';
+import logo from './assets/react.svg';
 import './App.css';
+import SassComponent from './component/SassComponent';
 
-function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} className="logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <SassComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
