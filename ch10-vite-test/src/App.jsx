21a1702a07a76778dec617af3d6801ba87634058
“Ihan0316” <aijnjj12@gmail.com>
2025-01-20 17:30:10 +0900

    25/1/20 8교시

--- ch10-vite-test/src/App.jsx
+++ ch10-vite-test/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <div></div>
+      <h2 className="react">ch 10 일정 관리 애플리케이션 예제</h2>
+      <div>Todo App을 만들자!</div>
     </>
   );
 }
