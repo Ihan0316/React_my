bd715b81d3272a3c4099bd0fc61afecf7f455f5c
“Ihan0316” <aijnjj12@gmail.com>
2025-01-20 14:30:56 +0900

    25/1/20 5교시

--- ch9-vite-test/src/App.jsx
+++ ch9-vite-test/src/App.jsx
@@ -1,27 +1,16 @@
-import { Component } from 'react';
-import './App.css';
-import CSSModule from './component/CSSModule';
+import React, { Component } from 'react';
+
+import CSSModule from './components/CSSModule';
+import SassComponent from './components/SassComponent';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <header>
-          <img src={logo} className="logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <SassComponent /> */}
-        <CSSModule />
+      <div>
+        <SassComponent />
+        {/* <CSSModule /> */}
       </div>
     );
   }
