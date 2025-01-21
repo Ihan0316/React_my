7b7a553bb96c8941bb1456b2f2987adeab335f28
“Ihan0316” <aijnjj12@gmail.com>
2025-01-21 10:01:29 +0900

    25/1/21 1교시

--- ch10-vite-test/src/App.jsx
+++ ch10-vite-test/src/App.jsx
@@ -1,10 +1,49 @@
+import { useState, useRef, useCallback } from 'react';
 import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  // 고유 id로 사용될 값
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo)); // 새로운 항목 추가
+    nextId.current += 1; // nextId를 1씩 증가
+  }, []);
+
   return (
     <>
-      <h2 className="react">ch 10 일정 관리 애플리케이션 예제</h2>
-      <div>Todo App을 만들자!</div>
+      <h2 className="react">ch10 일정 관리 애플리케이션 예제</h2>
+      <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} />
+      </TodoTemplate>
     </>
   );
 }
