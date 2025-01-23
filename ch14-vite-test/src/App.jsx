32ef386622bec4af94dc3ee9b988f6506742e8fe
“Ihan0316” <aijnjj12@gmail.com>
2025-01-23 10:24:52 +0900

    25/1/23 2교시

--- ch14-vite-test/src/App.jsx
+++ ch14-vite-test/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css';
+import NewsList from './components/NewsList';
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -7,16 +9,20 @@ const App = () => {
   const onClick = () => {
     axios
       .get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=a7387d99fe71491db9a957fc416280e9',
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f',
       )
       .then((response) => {
         setData(response.data);
       });
+    // const response = await axios.get(
+    //   'https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f'
+    // );
   };
 
   return (
     <div>
-      <div>
+      <h1 className="react">ch14 API 연동해서 뷰어 만들기</h1>
+      {/* <div>
         <button onClick={onClick}>불러오기</button>
       </div>
       {data && (
@@ -25,7 +31,8 @@ const App = () => {
           value={JSON.stringify(data, null, 2)}
           readOnly={true}
         />
-      )}
+      )} */}
+      <NewsList />
     </div>
   );
 };
